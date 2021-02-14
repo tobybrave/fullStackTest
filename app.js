d2a94551f7ccfa006fb2d8ea82512bddf9009124
tobybrave <leutoby@gmail.com>
2021-02-14 10:35:05 +0100

    deployment status

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
+
+app.get('/check', (request, response) => {
+  response.send('ok')
+})
+
+app.get('/version', (request, response) => {
+  response.send('1')
+})
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+  console.log("server started on port 5000");
+});
+
