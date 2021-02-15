527b91f4612257942c9a2b221e7d94a3119df4de
tobybrave <leutoby@gmail.com>
2021-02-15 02:23:15 +0100

    test: second rollback simulation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (request, response) => {
-  response.send('ok');
+  response.send('ok')
 })
 
 app.get('/version', (request, response) => {
